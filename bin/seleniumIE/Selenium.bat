ce54a90fc72057a3e0841d73f7b28278afe1b7de
FroggDev <admin@frogg.fr>
2019-12-17 09:56:26 +0100

    added chrome driver in driver list for selenium

--- bin/seleniumIE/Selenium.bat
+++ bin/seleniumIE/Selenium.bat
@@ -3,10 +3,11 @@
 set seleniumFolder=%~dp0
 
 set selenium="%seleniumFolder%\selenium-server-standalone-2.46.0.jar"
-set idDriver="%seleniumFolder%\IEDriverServer.exe"
+set ieDriver="%seleniumFolder%\IEDriverServer.exe"
+set chromeDriver="%seleniumFolder%\chromedriver.exe"
 
 IF EXIST %selenium% (
-java -Dwebdriver.ie.driver=%idDriver% -jar %selenium%
+java -Dwebdriver.chrome.driver=%chromeDriver% -Dwebdriver.ie.driver=%ieDriver% -jar %selenium%
 ) else (
 echo file %selenium% not found.
 REM Make a pause
